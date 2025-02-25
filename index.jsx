0c5c28c6cc72f9300285faf12f8d2d08e01d611c
bedirhan37code <bedirhankoca01@gmail.com>
2025-02-25 13:18:47 +0300

    first commit

---
+++ index.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { ProductContext } from "../../context/productContext";
+
+import Card from "../../components/Card";
+
+const Home = () => {
+  //Products contexdeki verilere eriş
+  const { products, selectedCategory } = useContext(ProductContext);
+
+  return (
+    <div className="mt-4 container">
+      <h1>{products.length} Ürün Bulundu</h1>
+      <h1>
+        {selectedCategory != "all" &&
+          selectedCategory + "  için sonuçlar bulundu"}{" "}
+      </h1>
+
+      <div className="wrapper">
+        {products.map((product) => (
+          <Card key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/HocExample/App copy.jsx
@@ -0,0 +1,12 @@
+import FormEleman from "./FormEleman";
+
+const App = () => {
+  return (
+    <div>
+      <h1>App</h1>
+      <Field />
+    </div>
+  );
+};
+
+export default App;
